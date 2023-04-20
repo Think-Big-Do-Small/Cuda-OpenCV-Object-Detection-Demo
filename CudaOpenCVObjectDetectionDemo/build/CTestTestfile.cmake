b8b0ad845b8552a42bcacc35b88b2025eaebd75b
文方俊 <89647356+Think-Big-Do-Small@users.noreply.github.com>
2023-04-20 15:52:24 +0800

    update at 2023-04-20

---
+++ CudaOpenCVObjectDetectionDemo/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/fangj/cmake_tutorial/CudaOpenCVObjectDetectionDemo
+# Build directory: C:/Users/fangj/cmake_tutorial/CudaOpenCVObjectDetectionDemo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
